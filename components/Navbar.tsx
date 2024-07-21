8a2a463f1f33020eccd76729e4de5e7584616cee
AbhinavMangalore16 <101643788+AbhinavMangalore16@users.noreply.github.com>
2024-07-21 22:19:29 +0530

    Navbar complete

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,30 +1,78 @@
 "use client";
 import { cn } from "@/lib/utils";
+import { Code, Images, LayoutDashboard, MessageCircleMore, Music, Settings, Video } from "lucide-react";
 
 import { Poppins } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
 
+const routes = [
+    {
+        label: "Dashboard",
+        icon: LayoutDashboard,
+        href: "/dashboard",
+        color: "text-sky-600"
+    },
+    {
+        label: "Chatbot",
+        icon: MessageCircleMore,
+        href: "/dashboard",
+        color: "text-[#38B2AC]",
+    },
+    {
+        label: "Image Generation",
+        icon: Images,
+        href: "/dashboard",
+        color: "text-[#7C4DFF]"
+    },{
+        label: "Code Generation",
+        icon: Code,
+        href: "/dashboard",
+        color: "text-[#A0D6E2]"
+    },{
+        label: "Video Generation",
+        icon: Video,
+        href: "/dashboard",
+        color: "text-[#4CAF50]"
+    },{
+        label: "Music Generation",
+        icon: Music,
+        href: "/dashboard",
+        color: "text-[#F4511E]"
+    },{
+        label: "Settings",
+        icon: Settings,
+        href: "/dashboard",
+        color: "text-[#808080]"
+    }
+];
+
 const poppins = Poppins({
     weight: "600",
-    subsets: ["latin"]
-    
-})
+    subsets: ["latin"],
+});
 
-const Navbar = () =>{
+const Navbar = () => {
     return (
-        <div className="flex flex-col h-full bg-[#1A1A2E]
-        text-white space-y-4 py-4">
+        <div className="flex flex-col h-full bg-[#1A1A2E] text-white space-y-4 py-4">
             <div className="flex-1 py-2 px-3">
                 <Link href="/dashboard" className="flex items-center pl-3 mb-14">
-                <div className="relative w-16 h-16 mr-1">
-                    <Image fill alt="Libra logo!" src="/libra_logo.png"/>
-                </div>
-                <h1 className={cn("text-2xl font-bold text-[#D3D3D3]", poppins.className)}>Libra</h1>
+                    <div className="relative w-16 h-16 mr-1">
+                        <Image fill alt="Libra logo!" src="/libra_logo.png" />
+                    </div>
+                    <h1 className={cn("text-2xl font-bold text-[#D3D3D3]", poppins.className)}>Libra</h1>
                 </Link>
+                <div className="space-y-1">
+                    {routes.map((route) => (
+                        <Link href={route.href} key={route.href} className="flex text-base group w-full justify-start font-medium cursor-pointer p-2 hover:bg-white/20 text-white rounded-lg transition">
+                            <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
+                            {route.label}
+                        </Link>
+                    ))}
+                </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default Navbar;
